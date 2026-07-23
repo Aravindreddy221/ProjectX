9e087e34408a3ff82b54e75f77ea8b3aed3c3d54
Aravindreddy221 <aravindhreddyk99@gmail.com>
2026-07-23 09:56:43 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PartnerSSODemo from "./mock.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PartnerSSODemo />
+  </React.StrictMode>
+);
